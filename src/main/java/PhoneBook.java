9b0b5d9b631fd708dc6ce74d2439fec5b8334e0a
Red <red.beard@bk.ru>
2018-08-08 22:13:56 +0300

    create PhoneBook.java

---
+++ src/main/java/PhoneBook.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class PhoneBook{
+
+		private static PhoneBook instance;
+
+		private PhoneBook(){}
+
+		public static PhoneBook get(){
+			
+			if (instance == null){
+				instance = new PhoneBook();
+			}
+
+			return instance;
+		}
+
+		public HashMap getBook(){
+			ArrayList <String> phonesIvanov = new ArrayList<>();
+			phonesIvanov.add("+8 800 2000 500");
+			phonesIvanov.add("+8 800 200 600");
+			ArrayList <String> phonesPetrov = new ArrayList<>();
+			phonesPetrov.add("+8 800 2000 700");
+			ArrayList <String> phonesSidorov = new ArrayList<>();
+			phonesSidorov.add("+8 800 2000 800");
+			phonesSidorov.add("+8 800 2000 900");
+			phonesSidorov.add("+8 800 2000 000");
+
+			HashMap<String, ArrayList<String>> personMap = new HashMap<>();
+			personMap.put("Иванов И.И.", phonesIvanov);
+			personMap.put("Петров П.П.", phonesPetrov);
+			personMap.put("Сидоров С.С.", phonesSidorov);
+			
+			return personMap;
+		}
+}
