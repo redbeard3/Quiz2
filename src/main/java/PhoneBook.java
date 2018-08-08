c98e752ff02e81136dd48291d40eb5f82f0e2850
Red <red.beard@bk.ru>
2018-08-08 22:29:33 +0300

    change PhoneBook because maven error
    Compilation failure /... /PhoneBook.java:[20,73] diamond operator is not supported in -source 1.5

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -17,17 +17,17 @@ public class PhoneBook{
 		}
 
 		public HashMap getBook(){
-			ArrayList <String> phonesIvanov = new ArrayList<>();
+			ArrayList <String> phonesIvanov = new ArrayList();
 			phonesIvanov.add("+8 800 2000 500");
 			phonesIvanov.add("+8 800 200 600");
-			ArrayList <String> phonesPetrov = new ArrayList<>();
+			ArrayList <String> phonesPetrov = new ArrayList();
 			phonesPetrov.add("+8 800 2000 700");
-			ArrayList <String> phonesSidorov = new ArrayList<>();
+			ArrayList <String> phonesSidorov = new ArrayList();
 			phonesSidorov.add("+8 800 2000 800");
 			phonesSidorov.add("+8 800 2000 900");
 			phonesSidorov.add("+8 800 2000 000");
 
-			HashMap<String, ArrayList<String>> personMap = new HashMap<>();
+			HashMap<String, ArrayList<String>> personMap = new HashMap();
 			personMap.put("Иванов И.И.", phonesIvanov);
 			personMap.put("Петров П.П.", phonesPetrov);
 			personMap.put("Сидоров С.С.", phonesSidorov);
