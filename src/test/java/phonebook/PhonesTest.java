6cbc221106ce87d231a94fcbdb87497da6d13976
Red <red.beard@bk.ru>
2018-08-10 16:10:29 +0300

    add new method for testing

--- src/test/java/phonebook/PhonesTest.java
+++ src/test/java/phonebook/PhonesTest.java
@@ -1,4 +1,6 @@
 
+package phonebook;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
@@ -10,22 +12,31 @@ public class PhonesTest{
 	private static Phones phones;
 
 	@Before
-	public static void initPhones(){
+	public void initPhones(){
 		phones = new Phones();
 	}
 
 	@After
-	public static void clearPhones(){
+	public void clearPhones(){
 		phones = null;
 	}
 
 	@Test
 	public void getPhoneListTest(){
 		ArrayList<String> list = new ArrayList<String>();
-		list.add("+8 800 2000 500");
-		list.add("+8 800 200 600");
+		list.add("1. +8 800 2000 500");
+		list.add("2. +8 800 200 600");
 		ArrayList<String> expected = list;
 		ArrayList<String> actual = phones.getPhoneList("Иванов И.И.");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getPhoneListTwoTest(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Такого ФИО нет в базе данных.");
+		ArrayList<String> expected = list;
+		ArrayList<String> actual = phones.getPhoneList("Васильков А.А.");
 		assertEquals(expected, actual);
 	}
 }
